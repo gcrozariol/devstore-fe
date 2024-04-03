317fd3bbc5dcda89efdd1948e76dcf2a506c49fd
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2024-04-03 15:12:44 -0300

    style(tailwind): add prettier rules for tailwindcss

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+    <div className="grid-rows-6 grid max-h-[860px] grid-cols-9 gap-6">
       <Link
         href="/"
-        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center"
+        className="group relative col-span-6 row-span-6 flex justify-center overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
           src="/moletom-ai-side.png"
@@ -14,11 +14,11 @@ export default function Home() {
           height={920}
           quality={100}
           alt=""
-          className="group-hover:scale-105 transition-transform duration-500"
+          className="transition-transform duration-500 group-hover:scale-105"
         />
 
-        <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom AI Side</span>
+        <div className="absolute bottom-28 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom AI Side</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
             USD 129
           </span>
@@ -27,7 +27,7 @@ export default function Home() {
 
       <Link
         href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center"
+        className="group relative col-span-3 row-span-3 flex justify-center overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
           src="/moletom-java.png"
@@ -35,11 +35,11 @@ export default function Home() {
           height={920}
           quality={100}
           alt=""
-          className="group-hover:scale-105 transition-transform duration-500"
+          className="transition-transform duration-500 group-hover:scale-105"
         />
 
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom Java</span>
+        <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom Java</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
             USD 119
           </span>
@@ -48,7 +48,7 @@ export default function Home() {
 
       <Link
         href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center"
+        className="group relative col-span-3 row-span-3 flex justify-center overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
           src="/moletom-ia-p-devs.png"
@@ -56,11 +56,11 @@ export default function Home() {
           height={920}
           quality={100}
           alt=""
-          className="group-hover:scale-105 transition-transform duration-500"
+          className="transition-transform duration-500 group-hover:scale-105"
         />
 
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="text-sm truncate">Moletom AI 4 Devs</span>
+        <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom AI 4 Devs</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
             USD 109
           </span>
