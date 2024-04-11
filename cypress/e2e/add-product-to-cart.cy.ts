f96391158a161fd98ce6087cb8af376b3c5bf17a
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2024-04-11 15:55:28 -0300

    test(cypress): setup custom searh by query command

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -24,7 +24,8 @@ describe('add product to card', () => {
   })
 
   it('should be able to search for a product and add it to the cart', () => {
-    cy.get('input[name=q]').type('moletom').parent('form').submit()
+    cy.searchByQuery('moletom')
+
     cy.get('a[href^="product"]').first().click()
 
     cy.location('pathname').should('include', '/product')
