6ea9b609105426db6b220c60ba07b119e49f69c6
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2024-04-11 16:23:17 -0300

    fix(build): add suspense wrapper to components using use search params

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -2,8 +2,9 @@
 
 import { Skeleton } from '@/components/skeleton'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
-export default function SearchLoading() {
+function SearchLoadingComponent() {
   const searchParams = useSearchParams()
 
   const query = searchParams.get('q')
@@ -23,5 +24,13 @@ export default function SearchLoading() {
         <Skeleton className="h-[400px]" />
       </div>
     </div>
+  )
+}
+
+export default function SearchLoading() {
+  return (
+    <Suspense>
+      <SearchLoadingComponent />
+    </Suspense>
   )
 }
